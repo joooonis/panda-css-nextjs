766b25c5205a4fed411ea27521981d045dd48dd2
joooonis <xowns3213@naver.com>
2024-06-23 18:37:52 +0900

    ✨ feat : patterns 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,15 @@
-import { center, flex } from "@/styled-system/patterns";
+"use client";
 
-const flexCol = flex({
-  direction: "column",
-  justify: "center",
-  align: "center",
-  bg: "primary.300",
-});
+import { flex as flexBase } from "@/styled-system/patterns";
+
+const flex = (diection: "row" | "col") =>
+  flexBase({
+    direction: diection,
+    justify: "center",
+    align: "center",
+    bg: "primary.300",
+  });
 
 export default function Home() {
-  return (
-    <div className={flexCol}>
-      Hello 🐼!
-      <p
-        className={center({
-          bg: "primary.900",
-          p: 36,
-          m: 12,
-          borderRadius: "xxl",
-          color: "white",
-          fontSize: 24,
-        })}
-      >
-        Center
-      </p>
-    </div>
-  );
+  return <div className={flex("col")}>Hello 🐼!</div>;
 }
