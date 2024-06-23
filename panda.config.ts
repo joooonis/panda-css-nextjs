9244f14ae265fb6f988607b4d09b0aabed850a97
joooonis <xowns3213@naver.com>
2024-06-24 01:01:15 +0900

    ✨ feat : shadow-panda ui kit

--- panda.config.ts
+++ panda.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
+  presets: ["@shadow-panda/preset"],
   preflight: true,
+  jsxFramework: "react",
+  emitPackage: true,
 
   include: [
     "./src/components/**/*.{ts,tsx,js,jsx}",
@@ -11,27 +14,41 @@ export default defineConfig({
   exclude: [],
 
   theme: {
-    tokens: {
-      colors: {
-        primary: {
-          100: { value: "#DCFCD4" },
-          200: { value: "#B3F9AB" },
-          300: { value: "#80EF7E" },
-          400: { value: "#80EF7E" },
-          500: { value: "#5CE067" },
-          600: { value: "#20AF46" },
-          700: { value: "#169243" },
-          800: { value: "#169243" },
-          900: { value: "#086139" },
+    extend: {
+      tokens: {
+        colors: {
+          primary: {
+            100: { value: "#DCFCD4" },
+            200: { value: "#B3F9AB" },
+            300: { value: "#80EF7E" },
+            400: { value: "#80EF7E" },
+            500: { value: "#5CE067" },
+            600: { value: "#20AF46" },
+            700: { value: "#169243" },
+            800: { value: "#169243" },
+            900: { value: "#086139" },
+          },
+        },
+        radii: {
+          sm: { value: "4px" },
+          md: { value: "8px" },
+          lg: { value: "16px" },
+          xl: { value: "24px" },
+          xxl: { value: "32px" },
+          full: { value: "9999px" },
         },
       },
-      radii: {
-        sm: { value: "4px" },
-        md: { value: "8px" },
-        lg: { value: "16px" },
-        xl: { value: "24px" },
-        xxl: { value: "32px" },
-        full: { value: "9999px" },
+      semanticTokens: {
+        colors: {
+          primary: {
+            DEFAILT: {
+              value: {
+                base: "primary.500",
+                _dark: "primary.100",
+              },
+            },
+          },
+        },
       },
     },
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import { flex as flexBase } from "@/styled-system/patterns";
-import { useState } from "react";
-
-const flex = (isBlack: boolean) =>
-  flexBase({
-    direction: "column",
-    justify: "center",
-    align: "center",
-    bg: "primary.300",
-    color: isBlack ? "black" : "white",
-  });
+import { container, hstack } from "@/styled-system/patterns";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 export default function Home() {
-  const [isBlack, setIsBlack] = useState(false);
   return (
-    <div onClick={() => setIsBlack(!isBlack)} className={flex(isBlack)}>
-      Hello 🐼!
+    <div
+      className={container({
+        maxW: "500px",
+        my: "24px",
+      })}
+    >
+      <div
+        className={hstack({
+          w: "full",
+          gap: "4",
+        })}
+      >
+        <Input type="email" placeholder="Email" />
+        <Button>Save</Button>
+      </div>
     </div>
   );
 }
